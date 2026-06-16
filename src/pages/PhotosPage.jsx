6387390567fbd3b8c6256624e0959a64e6aaa1af
arunoruberuto <arunoruberuto@gmail.com>
2026-06-16 17:19:20 +0900

    パス変更

--- src/pages/PhotosPage.jsx
+++ src/pages/PhotosPage.jsx
@@ -18,7 +18,7 @@ export function PhotosPage() {
             </div>
           </div>
           <MediaFrame
-            src={`${import.meta.env.BASE_URL}images/placeholders/wide.svg`}
+            src="/images/placeholders/wide.svg"
             alt="Photography page main image placeholder"
             aspect="wide"
             caption="写真ページのコンセプトと紹介テキスト / イメージ画像"
