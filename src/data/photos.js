1109b0fea6f0c22d91cc50d9a423b9be463943d3
shou <shou2409jp@gmail.com>
2026-07-09 14:39:44 +0900

    写真

--- src/data/photos.js
+++ src/data/photos.js
@@ -91,5 +91,282 @@ export const photos = [
     caption: "Image from public/images/photos/013.JPG.",
     image: `${base}images/photos/013.JPG`,
     orientation: "landscape"
-  }
+  },
+  {
+    id: "photo-014",
+    title: "Photo 014",
+    caption: "Image from public/images/photos/014.jpg.",
+    image: `${base}images/photos/014.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-015",
+    title: "Photo 015",
+    caption: "Image from public/images/photos/015.jpg.",
+    image: `${base}images/photos/015.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-016",
+    title: "Photo 016",
+    caption: "Image from public/images/photos/016.jpg.",
+    image: `${base}images/photos/016.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-017",
+    title: "Photo 017",
+    caption: "Image from public/images/photos/017.jpg.",
+    image: `${base}images/photos/017.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-018",
+    title: "Photo 018",
+    caption: "Image from public/images/photos/018.jpg.",
+    image: `${base}images/photos/018.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-019",
+    title: "Photo 019",
+    caption: "Image from public/images/photos/019.jpeg.",
+    image: `${base}images/photos/019.jpeg`,
+    orientation: "landscape"
+  },{
+    id: "photo-020",
+    title: "Photo 020",
+    caption: "Image from public/images/photos/020.jpg.",
+    image: `${base}images/photos/020.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-021",
+    title: "Photo 021",
+    caption: "Image from public/images/photos/021.jpg.",
+    image: `${base}images/photos/021.jpg`,
+    orientation: "landscape"
+  },{
+    id: "photo-022",
+    title: "Photo 022",
+    caption: "Image from public/images/photos/022.jpeg.",
+    image: `${base}images/photos/022.jpeg`,
+    orientation: "landscape"
+  },{
+    id: "photo-023",
+    title: "Photo 023",
+    caption: "Image from public/images/photos/023.jpeg.",
+    image: `${base}images/photos/023.jpeg`,
+    orientation: "landscape"
+  },{
+    id: "photo-024",
+    title: "Photo 024",
+    caption: "Image from public/images/photos/024.jpeg.",
+    image: `${base}images/photos/024.jpeg`,
+    orientation: "landscape"
+  },{
+    id: "photo-025",
+    title: "Photo 025",
+    caption: "Image from public/images/photos/025.jpeg.",
+    image: `${base}images/photos/025.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-026",
+    title: "Photo 026",
+    caption: "Image from public/images/photos/026.jpeg.",
+    image: `${base}images/photos/026.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-027",
+    title: "Photo 027",
+    caption: "Image from public/images/photos/027.jpeg.",
+    image: `${base}images/photos/027.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-028",
+    title: "Photo 028",
+    caption: "Image from public/images/photos/028.jpeg.",
+    image: `${base}images/photos/028.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-029",
+    title: "Photo 029",
+    caption: "Image from public/images/photos/029.jpeg.",
+    image: `${base}images/photos/029.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-030",
+    title: "Photo 030",
+    caption: "Image from public/images/photos/030.jpeg.",
+    image: `${base}images/photos/030.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-031",
+    title: "Photo 031",
+    caption: "Image from public/images/photos/031.jpeg.",
+    image: `${base}images/photos/031.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-032",
+    title: "Photo 032",
+    caption: "Image from public/images/photos/032.jpeg.",
+    image: `${base}images/photos/032.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-033",
+    title: "Photo 033",
+    caption: "Image from public/images/photos/033.jpeg.",
+    image: `${base}images/photos/033.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-034",
+    title: "Photo 034",
+    caption: "Image from public/images/photos/034.jpeg.",
+    image: `${base}images/photos/034.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-035",
+    title: "Photo 035",
+    caption: "Image from public/images/photos/035.jpeg.",
+    image: `${base}images/photos/035.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-036",
+    title: "Photo 036",
+    caption: "Image from public/images/photos/036.jpeg.",
+    image: `${base}images/photos/036.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-037",
+    title: "Photo 037",
+    caption: "Image from public/images/photos/037.jpeg.",
+    image: `${base}images/photos/037.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-038",
+    title: "Photo 038",
+    caption: "Image from public/images/photos/038.png.",
+    image: `${base}images/photos/038.png`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-039",
+    title: "Photo 039",
+    caption: "Image from public/images/photos/039.jpeg.",
+    image: `${base}images/photos/039.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-040",
+    title: "Photo 040",
+    caption: "Image from public/images/photos/040.jpeg.",
+    image: `${base}images/photos/040.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-041",
+    title: "Photo 041",
+    caption: "Image from public/images/photos/041.jpg.",
+    image: `${base}images/photos/041.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-042",
+    title: "Photo 042",
+    caption: "Image from public/images/photos/042.jpg.",
+    image: `${base}images/photos/042.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-043",
+    title: "Photo 043",
+    caption: "Image from public/images/photos/043.jpeg.",
+    image: `${base}images/photos/043.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-044",
+    title: "Photo 044",
+    caption: "Image from public/images/photos/044.jpeg.",
+    image: `${base}images/photos/044.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-045",
+    title: "Photo 045",
+    caption: "Image from public/images/photos/045.jpg.",
+    image: `${base}images/photos/045.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-046",
+    title: "Photo 046",
+    caption: "Image from public/images/photos/046.jpg.",
+    image: `${base}images/photos/046.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-047",
+    title: "Photo 047",
+    caption: "Image from public/images/photos/047.jpg.",
+    image: `${base}images/photos/047.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-048",
+    title: "Photo 048",
+    caption: "Image from public/images/photos/048.jpg.",
+    image: `${base}images/photos/048.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-049",
+    title: "Photo 049",
+    caption: "Image from public/images/photos/049.jpg.",
+    image: `${base}images/photos/049.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-050",
+    title: "Photo 050",
+    caption: "Image from public/images/photos/050.jpeg.",
+    image: `${base}images/photos/050.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-051",
+    title: "Photo 051",
+    caption: "Image from public/images/photos/051.jpeg.",
+    image: `${base}images/photos/051.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-052",
+    title: "Photo 052",
+    caption: "Image from public/images/photos/052.jpeg.",
+    image: `${base}images/photos/052.jpeg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-053",
+    title: "Photo 053",
+    caption: "Image from public/images/photos/053.jpg.",
+    image: `${base}images/photos/053.jpg`,
+    orientation: "landscape"
+  },
+  {
+    id: "photo-054",
+    title: "Photo 054",
+    caption: "Image from public/images/photos/054.jpeg.",
+    image: `${base}images/photos/054.jpeg`,
+    orientation: "landscape"
+  },
 ];
